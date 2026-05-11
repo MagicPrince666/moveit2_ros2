7bf2c418abdde683d867681f5cc4ee40afa638df
MagicPrince666 <846863428@qq.com>
2026-05-11 18:36:42 +0800

    防止重复导出

--- scripts/pca9685.sh
+++ scripts/pca9685.sh
@@ -2,10 +2,14 @@
 
 # 循环初始化16通道PWM (0-15)
 for i in {0..15}; do
-    echo $i > /sys/class/pwm/pwmchip16/export
+    # 如果已导出则跳过重复导出
+    if [ ! -d "/sys/class/pwm/pwmchip16/pwm$i" ]; then
+        echo $i > /sys/class/pwm/pwmchip16/export
+    fi
+
+    # echo inversed > /sys/class/pwm/pwmchip16/pwm$i/polarity
     echo 20000000 > /sys/class/pwm/pwmchip16/pwm$i/period
     echo 1500000 > /sys/class/pwm/pwmchip16/pwm$i/duty_cycle
-    echo invert > /sys/class/pwm/pwmchip16/pwm$i/polarity
     echo 1 > /sys/class/pwm/pwmchip16/pwm$i/enable
     # 设置权限
     chmod 666 /sys/class/pwm/pwmchip16/pwm$i/period
