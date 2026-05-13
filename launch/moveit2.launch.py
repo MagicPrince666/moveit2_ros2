a372dba973a0348bdb950c3c341844ca52a536fd
MagicPrince666 <846863428@qq.com>
2026-05-13 10:01:06 +0800

    删除无用节点

--- launch/moveit2.launch.py
+++ launch/moveit2.launch.py
@@ -20,10 +20,8 @@ def generate_launch_description():
     if ROS_DISTRO == 'humble' or ROS_DISTRO == 'galactic' or ROS_DISTRO == 'foxy' or ROS_DISTRO == 'iron':
         return LaunchDescription([
             Node(namespace='/', package='moveit2', executable='moveit2_node', name='moveit2_node', parameters=[moveit2_cfg], output='screen'),
-            Node(package='tf2_ros', executable='static_transform_publisher', arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','imu_link']),
         ])
     else:
         return LaunchDescription([
             Node(node_namespace='/', package='moveit2', node_executable='moveit2_node', node_name='moveit2_node', parameters=[moveit2_cfg], output='screen'),
-            Node(package='tf2_ros', node_executable='static_transform_publisher', arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','imu_link']),
         ])
